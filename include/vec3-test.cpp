abf4dfca013dfe7bbaf68ede0effc760e12f1ff8
Ueliton de Paula Freitas <ueliton_freitas@desktop.facom.ufms.br>
2014-09-12 14:02:33 -0400

    adicionando códigos

---
+++ include/vec3-test.cpp
@@ -0,0 +1,16 @@
+#include "Math/Vector3.h"
+
+using namespace Ds;
+
+typedef Vector3<float> vec3;
+
+int
+main(int argc, char* argv[])
+{
+  /*
+    INSERT YOUR CODE HERE
+  */
+  puts("Press any key to exit...");
+  getchar();
+  return 0;
+}
